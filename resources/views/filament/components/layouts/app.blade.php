40cc8b28a3ef511dd0333dc38c3a44112c85f5a3
nngery <nngery0@gmail.com>
2025-03-21 20:58:49 +0700

    Add favicon link to Filament app layout

--- resources/views/filament/components/layouts/app.blade.php
+++ resources/views/filament/components/layouts/app.blade.php
@@ -7,6 +7,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+        <link rel="icon" href="filament.admin.logo">
         <title>{{ config('app.name') }}</title>
 
         <style>
